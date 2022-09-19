9e91e04a0b222b9768c4dceb638a1c373da28f15
Metranohm <andrewwink@gmail.com>
2022-09-19 17:26:40 -0500

    initial commit

---
+++ data/students-data.js
@@ -0,0 +1,9 @@
+const students = [
+  {text: 'Bill Dautrive', enrolled: false, _id: 125223},
+  {text: 'John Redcorn', enrolled: false, _id: 127904},
+  {text: 'Bobby Hill', enrolled: true, _id: 139608},
+]
+
+export { 
+	students
+}
